d5c3f65b88addc6dcfef681216c11e537a78bb2a
Ludwig Högström <ludwig.hogstrom@bauermedia.se>
2023-04-03 13:26:09 +0200

    Comment about the error.

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -2,6 +2,9 @@ import { NextPage } from "next";
 import React from "react";
 
 const CustomError: NextPage = (props) => {
+  // We're getting the default Vercel error page instead of this custom error page
+  // when project is deployed on Vercel. It could have something to do with "i18n"
+  // in "next.config.js". Trigger the error via the "/test-error/error" page.
   console.log("_error props:", props)
   return <main><h1>Custom _error page</h1><p>Error</p></main>
 };
