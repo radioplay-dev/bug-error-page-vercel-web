6f6792981f9aecb027aba2472fef01584c08c9d7
Ludwig Högström <ludwig.hogstrom@bauermedia.se>
2023-04-03 13:19:31 +0200

    No need for `await` to trigger the error

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 import React from "react";
-//import {loadI18nDictionary} from "@/helpers/loadI18nDictionary";
 
 const CustomError: NextPage = (props) => {
   console.log("_error props:", props)
@@ -8,14 +7,8 @@ const CustomError: NextPage = (props) => {
 };
 
 CustomError.getInitialProps = async () => {
-  // Looks like we are getting the Vercel error page instead of "CustomError" page
-  // above when "i18n" is enabled in "next.config" and we're awaiting something in
-  // this "getInitialProps" function.
-  //const i18n = await loadI18nDictionary();
-
   return {
     foo: "bar",
-    i18n: {fff: "fff"},
   };
 };
 
